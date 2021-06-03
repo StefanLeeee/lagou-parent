165ed7d21c542ee8e3888d8ab2fee5cd4c3f2afe
stefan <1693625254@qq.com>
2021-06-03 09:22:36 +0800

    autodeliver Controller

--- lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
+++ lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
@@ -1,9 +1,26 @@
 package com.lagou.edu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * @author Zhang
  * @date 6/2/2021 10:35 AM
  * @description
  */
+
+@RestController
+@RequestMapping("/autodeliver")
 public class AutodeliverController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/checkState/{userId}")
+    public Integer findResumeOpenState() {
+        return 1;
+    }
 }
